d77b849251ef3ffff0267005d93c4836481c56bc
jt496 <john.talbot@gmail.com>
2022-07-14 16:43:31 +0100

    list version of example complete

--- src/turan1.lean
+++ src/turan1.lean
@@ -288,6 +288,43 @@ begin
   rw sum_degrees_eq_twice_card_edges at sdG, rw card_univ at sdG, rwa [mul_le_mul_left] at sdG, by norm_num,
 end
 
+--- for any t and n there is a list of (t+1) nats summing to n whose sum of squares is n^2 - 2*Turan(t+1,n)
+--- ie there is a partition of n into t+1 parts so that the multipartite graph on these parts has the maximum number of edges
+lemma turan_list_lb (t n : ℕ) : ∃l:list ℕ,l.length=t+1 ∧ l.sum=n ∧ n^2=(l.map (λi,i^2)).sum +2*turan_numb t n:=
+begin
+  revert n,
+  induction t with t ht,
+  intro n, rw [zero_add,tn_simp',mul_zero],
+  use [[n]],dsimp, simp only [eq_self_iff_true, list.sum_cons, list.sum_nil, add_zero, and_self] at *,
+  intro n,
+  cases nat.eq_zero_or_pos n with hn0,
+  rcases ht 0 with ⟨h0,h1,h2,h3⟩,
+  rw [tn_simp,mul_zero,zero_pow (by norm_num:0<2)] at h3,
+  set ml:= 0 :: h0,
+  use ml,
+  simp only [list.length, list.map, add_left_inj, list.sum_cons, zero_add, nat.nat_zero_eq_zero, zero_pow', ne.def, bit0_eq_zero,
+  nat.one_ne_zero, not_false_iff, add_zero], rw [hn0,zero_pow (by norm_num:0<2),tn_simp,mul_zero,add_zero ],
+  exact ⟨h1,h2,h3⟩,
+  have ms:=succ_pred_eq_of_pos h,
+  set m:=n.pred with hm,rw ← ms,
+----inductive step 
+  choose b hb1 hb2 using (tn_max_mem t m m ((self_mem_range_succ m))),
+  rcases ht b with ⟨l,l0,l1,l2⟩,
+  set tl:= (m+1-b) :: l,
+  use tl,
+  split, simp only [*, list.length] at *,
+  split, simp only [*, list.sum_cons, mem_range, eq_self_iff_true] at *, rw ← succ_eq_add_one, rw ms, 
+  rw [← succ_eq_add_one, ms] at hb1,
+  apply tsub_add_cancel_of_le (le_of_lt hb1),
+  rw [ms],
+  simp only [*, list.map, list.sum_cons, mem_range, eq_self_iff_true] at *,
+  rw [← succ_eq_add_one ,ms] at hb2,
+  rw ← hb2,rw mul_add,rw ← add_assoc, rw add_comm, rw add_assoc, rw ←l2,
+  clear_except hb1 ms, rw ← succ_eq_add_one at hb1,rw ms at hb1,
+  rw ← succ_eq_add_one,rw ms, 
+  have:= tsub_add_cancel_of_le (le_of_lt hb1),
+  nth_rewrite 0 ← this, ring,
+end
   
 ---- given a graph on a subset of α can form the bipartite join with rest of α
 def bip_join {A : finset α} (H: simple_graph A) : simple_graph α:={
@@ -309,11 +346,6 @@ end,
 loopless:=
 begin
   obviously,
-end,
-}
-/-
-def TG (n :ℕ): ℕ → simple_graph (range n)
-| 0  := { adj := λ i j, false }
-| (t+1):= TG (n-(Turan_part (t+1) n))
--/
+end,}
+
 end simple_graph
--- src/turan2.lean
+++ src/turan2.lean
@@ -41,69 +41,6 @@ begin
 end
 
 
-lemma mysucc (h: 0 <n) :∃m:ℕ, m.succ=n :=
-begin
-use n.pred, apply succ_pred_eq_of_pos h,
-end
-
-
-
-lemma maxlistsum (t n : ℕ) : ∃l:list ℕ,l.length=t+1 ∧ l.sum=n ∧ n^2=(l.map (λi,i^2)).sum +2*turan_numb t n:=
-begin
-  revert n,
-  induction t with t ht,
-  intro n, rw [zero_add,tn_simp',mul_zero],
-  use [[n]],dsimp, simp only [eq_self_iff_true, list.sum_cons, list.sum_nil, add_zero, and_self] at *,
-  intro n,
---  induction n using nat.strong_induction_on with n hn,
-  cases nat.eq_zero_or_pos n with hn0,
-  rcases ht 0 with ⟨h0,h1,h2,h3⟩,
-  rw [tn_simp,mul_zero,zero_pow (by norm_num:0<2)] at h3,
-  set ml:= 0 :: h0,
-  use ml,
-  simp only [list.length, list.map, add_left_inj, list.sum_cons, zero_add, nat.nat_zero_eq_zero, zero_pow', ne.def, bit0_eq_zero,
-  nat.one_ne_zero, not_false_iff, add_zero], rw [hn0,zero_pow (by norm_num:0<2),tn_simp,mul_zero,add_zero ],
-  exact ⟨h1,h2,h3⟩,
-  have ms:=succ_pred_eq_of_pos h,
-  set m:=n.pred with m,rw ← ms,
-----inductive step 
-sorry,
-end
-
-
-
-
-
-
--- form list [b,b,...b] of length a
-def mylist : ℕ → ℕ → list ℕ
-| 0   b   := []
-|(a+1)  b   :=  b :: mylist a b
-
-def addlist : list ℕ → list ℕ → list ℕ
-| [] _ := []
-| _ [] := []
-| (a :: l) (b :: m)  :=(a+b):: addlist l m
-
-
-
-
-
-
-
--- given t and n return the size of the next part in turan (t+1) partite graph of order n
-noncomputable theory
-def  Turan_part  : ℕ → ℕ → ℕ:=
-begin
-  intros t n,
-  by_cases hn: n=0, exact 0,
-  by_cases ht: n=0, exact  n,
-  choose m hm using nat.exists_eq_succ_of_ne_zero hn, 
-  choose s hs using nat.exists_eq_succ_of_ne_zero ht, 
-  choose b hb1 hb2 using (tn_max_mem s m m ((self_mem_range_succ m))),
-  exact b,
-end
-
 
 
 end simple_graph
