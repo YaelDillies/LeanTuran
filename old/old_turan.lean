1e2764e7fb9138ddc28c19ac5bb94a64a9595cd1
jt496 <john.talbot@gmail.com>
2022-07-26 14:55:59 +0100

    G.ind A degree in terms of G.deg_res A

--- old/old_turan.lean
+++ old/old_turan.lean
@@ -13,6 +13,163 @@ open finset nat
 open_locale big_operators 
 
 namespace simple_graph
+-- t_n t n is the maximum number of edges in a (t+2)-clique free graph of order n
+-- or equivalently the maximum numb of edges in a complete (t+1)-partite graph order n
+-- or equivalently .... 
+def t_n : ℕ → ℕ → ℕ:=
+begin
+  intros t n,
+  set a:= n/(t+1),--- size of a small part
+  set b:= n-(t+1)*a,-- number of large parts
+  exact (a^2)*nat.choose(t+1-b)(2)+a*(a+1)*b*(t+1-b)+((a+1)^2)*nat.choose(b)(2),
+end
+
+
+--variables {t n : ℕ} 
+-- turan_numb t n is max numb of edges in an n vertex t+1 partite graph 
+---what about 0-partite? sorry not going there...
+def turan_numb : ℕ → ℕ → ℕ
+| _       0 := 0
+| 0       _ := 0
+| (t+1) (n+1) :=option.get_or_else ((range(n+1)).image(λa, turan_numb t a + a*(n+1-a))).max 0 
+
+
+lemma tn_simp (t :ℕ): turan_numb t 0 = 0 := by cases t;refl
+
+lemma tn_simp' (n :ℕ): turan_numb 0 n = 0 := by cases n;refl
+
+--the turan number is the maximum of the lhs below 
+lemma tn_le (t c n : ℕ) (h: c ∈ range (n+1)): turan_numb t c +c*(n+1-c) ≤ turan_numb (t+1) (n+1):=
+begin
+  obtain ⟨x, hx : _ = _⟩ := finset.max_of_mem (mem_image_of_mem (λa, turan_numb t a + a*(n+1-a)) h),
+  have := finset.le_max_of_mem (mem_image_of_mem _ h) hx,
+  rw [turan_numb, hx], apply le_trans this,refl,
+end
+
+-- obtain the optimal choice of size of new part in turan graph T (t+1)(n+1)
+lemma tn_max_mem (t c n : ℕ) (h: c∈ range (n+1)) : ∃ b ∈ range(n+1), turan_numb t b +b*(n+1-b) = turan_numb (t+1) (n+1):=
+begin
+  obtain ⟨i, hi : _ = _⟩ := finset.max_of_mem (mem_image_of_mem (λa, turan_numb t a + a*(n+1-a)) h),
+  have := mem_of_max  hi,
+  rw mem_image at this,
+  rcases this with ⟨b,hbr,hbm⟩,
+  use [b,hbr],
+  rw [turan_numb, hi, hbm],refl,
+end
+
+
+variables {t n : ℕ} 
+variables {α : Type*} (G : simple_graph α)[fintype α][inhabited α]{s : finset α}[decidable_eq α][decidable_rel G.adj]
+
+lemma turan_bd (s : ℕ) {A B :finset α} (hA: A.nonempty) (hB: B⊆A) (hB': B⊂ A): turan_numb s B.card + B.card * (A\B).card ≤ turan_numb s.succ A.card:=
+begin
+  have : A.card = B.card +(A\B).card,{
+    convert card_disjoint_union _, exact (union_sdiff_of_subset hB).symm , apply sdiff_disjoint.symm,},
+  rw add_comm at this, 
+  obtain ⟨c,hc⟩:=nat.exists_eq_succ_of_ne_zero (ne_of_gt (finset.card_pos.mpr hA)), 
+  rw [hc, nat.succ_eq_add_one] at this,
+  convert tn_le s B.card _ _,
+  exact (tsub_eq_of_eq_add this).symm,
+  rw [mem_range,  ← nat.succ_eq_add_one, ← hc],
+  exact card_lt_card hB',
+end
+
+
+-- main theorem basically erdos proof of degree majorisation of (t+2)-clique-free graph by (t+1)-partite graph
+theorem erdos_simple : ∀A:finset α, G.clique_free_set A (t+2) → ∑ v in A,G.deg_res v A ≤ 2*(turan_numb t A.card):=
+begin
+  induction t with s hs, intros A hA,
+  rw [tn_simp',mul_zero], rw zero_add at hA, 
+  have := G.two_clique_free hA, 
+  rwa [nonpos_iff_eq_zero, sum_eq_zero_iff], 
+  -- s.succ case 
+  intros A hA, 
+  by_cases hnem: A.nonempty,{
+    obtain ⟨x,hxA,hxM⟩:=G.exists_max_res_deg_vertex hnem, -- get a vert x of max res deg in A
+    -- Let B be the res nbhd of the vertex x of max deg_A 
+    set B:= (G.nbhd_res x A), 
+    have hBA: B⊆ A:=(G.sub_res_nbhd_A x A), -- B is contained in A
+    -- split sum of deg_A using both (1) deg_A v = deg_B v + deg_(A\B) v and (2) the partition: A = B ∪ (A\B)
+    -- have ∑ v ∈ A, deg_A v = ∑ v ∈ B, deg_B v + ∑ v ∈ A\B, deg_B v + ∑ v ∈ A, deg_(A\B) v
+    rw [G.deg_res_add_sum hBA ,G.sum_sdf hBA hBA, add_assoc],
+    -- apply inductive hyp to ∑ v ∈ B, deg_B v using the fact that 
+    -- res nbhd of x to B = is (s+2)-clique free (since A is (s.succ+2)-clique free)
+    apply add_le_of_add_le_right _ (hs (G.nbhd_res x A) (G.t_clique_free hA hxA)),
+    -- use ∑ v ∈ A, deg_(A\B) v = ∑ v ∈ A\B, deg_A v
+    rw [G.sum_sdf hBA (sdiff_subset A B),G.bip_count hBA,← G.deg_res_add_sum hBA ],
+    nth_rewrite 1 add_comm, rw ← add_assoc,
+    -- the next line is a strict overestimate if A\B contains any edges
+    -- since we replace ∑ v ∈ A\B, deg_B v by ∑ v ∈ A\B, deg_A v (i.e. we add in ∑ v∈ A\B, deg_(A\B) v) 
+    apply add_le_of_add_le_left _ (G.sum_res_le hBA (sdiff_subset A B)),
+    ring_nf, rw ← mul_add, refine mul_le_mul' _ _, refl,
+    -- now overestimate by assuming all v ∈ A\B have max deg (i.e. assuming G[A\B,B] is complete bipartite )
+    apply add_le_of_add_le_left _ (G.max_deg_res_sum_le (sdiff_subset A B)), 
+    rw [hxM,nbhd_res],
+    -- use our bound on Turan numbers 
+    exact turan_bd s hnem hBA (G.ssub_res_nbhd_of_mem hxA),},
+    -- ¬ A.nonempty ie A = ∅
+  { rw not_nonempty_iff_eq_empty at hnem, 
+    rw [hnem,finset.card_empty,turan_numb, mul_zero,finset.sum_empty],},
+end
+
+
+
+-- usual-ish statement of turan upper bound
+theorem turan_ub : G.clique_free (t+2) → G.edge_finset.card ≤ turan_numb t (fintype.card α):=
+begin
+  intro h,
+  have sdG:= G.erdos_simple univ (G.clique_free_graph_imp_set h),
+  simp only [deg_res_univ] at sdG,
+  rwa [sum_degrees_eq_twice_card_edges,card_univ,mul_le_mul_left] at sdG, by norm_num,
+end
+
+
+
+
+
+-- t_n t n is the maximum number of edges in a (t+2)-clique free graph of order n
+-- or equivalently the maximum numb of edges in a complete (t+1)-partite graph order n
+-- or equivalently .... 
+def t_n : ℕ → ℕ → ℕ:=
+begin
+  intros t n,
+  set a:= n/(t+1),--- size of a small part
+  set b:= n-(t+1)*a,-- number of large parts
+  exact (a^2)*nat.choose(t+1-b)(2)+a*(a+1)*b*(t+1-b)+((a+1)^2)*nat.choose(b)(2),
+end
+
+
+--variables {t n : ℕ} 
+-- turan_numb t n is max numb of edges in an n vertex t+1 partite graph 
+---what about 0-partite? sorry not going there...
+def turan_numb : ℕ → ℕ → ℕ
+| _       0 := 0
+| 0       _ := 0
+| (t+1) (n+1) :=option.get_or_else ((range(n+1)).image(λa, turan_numb t a + a*(n+1-a))).max 0 
+
+
+lemma tn_simp (t :ℕ): turan_numb t 0 = 0 := by cases t;refl
+
+lemma tn_simp' (n :ℕ): turan_numb 0 n = 0 := by cases n;refl
+
+--the turan number is the maximum of the lhs below 
+lemma tn_le (t c n : ℕ) (h: c ∈ range (n+1)): turan_numb t c +c*(n+1-c) ≤ turan_numb (t+1) (n+1):=
+begin
+  obtain ⟨x, hx : _ = _⟩ := finset.max_of_mem (mem_image_of_mem (λa, turan_numb t a + a*(n+1-a)) h),
+  have := finset.le_max_of_mem (mem_image_of_mem _ h) hx,
+  rw [turan_numb, hx], apply le_trans this,refl,
+end
+
+-- obtain the optimal choice of size of new part in turan graph T (t+1)(n+1)
+lemma tn_max_mem (t c n : ℕ) (h: c∈ range (n+1)) : ∃ b ∈ range(n+1), turan_numb t b +b*(n+1-b) = turan_numb (t+1) (n+1):=
+begin
+  obtain ⟨i, hi : _ = _⟩ := finset.max_of_mem (mem_image_of_mem (λa, turan_numb t a + a*(n+1-a)) h),
+  have := mem_of_max  hi,
+  rw mem_image at this,
+  rcases this with ⟨b,hbr,hbm⟩,
+  use [b,hbr],
+  rw [turan_numb, hi, hbm],refl,
+end
 
 
 -- inductive step for Erdos proof: if A is (t.succ+2)-clique free then for any v ∈ A the restricted nbhd of v in A is (t+2)-clique-free
@@ -83,5 +240,69 @@ begin
   rw [← hb2, mul_add, ← add_assoc, add_comm, add_assoc, ←l2],  
   nth_rewrite 0 ← (tsub_add_cancel_of_le (le_of_lt hb1)), ring,
 end
+
+
+-- main theorem basically erdos proof of degree majorisation of (t+2)-clique-free graph by (t+1)-partite graph
+theorem erdos_simple : ∀A:finset α, G.clique_free_set A (t+2) → ∑ v in A,G.deg_res v A ≤ 2*(turan_numb t A.card):=
+begin
+  induction t with s hs, intros A hA,
+  rw [tn_simp',mul_zero], rw zero_add at hA, 
+  have := G.two_clique_free hA, 
+  rwa [nonpos_iff_eq_zero, sum_eq_zero_iff], 
+  -- s.succ case 
+  intros A hA, 
+  by_cases hnem: A.nonempty,{
+    obtain ⟨x,hxA,hxM⟩:=G.exists_max_res_deg_vertex hnem, -- get a vert x of max res deg in A
+    -- Let B be the res nbhd of the vertex x of max deg_A 
+    set B:= (G.nbhd_res x A), 
+    have hBA: B⊆ A:=(G.sub_res_nbhd_A x A), -- B is contained in A
+    -- split sum of deg_A using both (1) deg_A v = deg_B v + deg_(A\B) v and (2) the partition: A = B ∪ (A\B)
+    -- have ∑ v ∈ A, deg_A v = ∑ v ∈ B, deg_B v + ∑ v ∈ A\B, deg_B v + ∑ v ∈ A, deg_(A\B) v
+    rw [G.deg_res_add_sum hBA ,G.sum_sdf hBA hBA, add_assoc],
+    -- apply inductive hyp to ∑ v ∈ B, deg_B v using the fact that 
+    -- res nbhd of x to B = is (s+2)-clique free (since A is (s.succ+2)-clique free)
+    apply add_le_of_add_le_right _ (hs (G.nbhd_res x A) (G.t_clique_free hA hxA)),
+    -- use ∑ v ∈ A, deg_(A\B) v = ∑ v ∈ A\B, deg_A v
+    rw [G.sum_sdf hBA (sdiff_subset A B),G.bip_count hBA,← G.deg_res_add_sum hBA ],
+    nth_rewrite 1 add_comm, rw ← add_assoc,
+    -- the next line is a strict overestimate if A\B contains any edges
+    -- since we replace ∑ v ∈ A\B, deg_B v by ∑ v ∈ A\B, deg_A v (i.e. we add in ∑ v∈ A\B, deg_(A\B) v) 
+    apply add_le_of_add_le_left _ (G.sum_res_le hBA (sdiff_subset A B)),
+    ring_nf, rw ← mul_add, refine mul_le_mul' _ _, refl,
+    -- now overestimate by assuming all v ∈ A\B have max deg (i.e. assuming G[A\B,B] is complete bipartite )
+    apply add_le_of_add_le_left _ (G.max_deg_res_sum_le (sdiff_subset A B)), 
+    rw [hxM,nbhd_res],
+    -- use our bound on Turan numbers 
+    exact turan_bd s hnem hBA (G.ssub_res_nbhd_of_mem hxA),},
+    -- ¬ A.nonempty ie A = ∅
+  { rw not_nonempty_iff_eq_empty at hnem, 
+    rw [hnem,finset.card_empty,turan_numb, mul_zero,finset.sum_empty],},
+end
+
+
+lemma turan_bd (s : ℕ) {A B :finset α} (hA: A.nonempty) (hB: B⊆A) (hB': B⊂ A): turan_numb s B.card + B.card * (A\B).card ≤ turan_numb s.succ A.card:=
+begin
+  have : A.card = B.card +(A\B).card,{
+    convert card_disjoint_union _, exact (union_sdiff_of_subset hB).symm , apply sdiff_disjoint.symm,},
+  rw add_comm at this, 
+  obtain ⟨c,hc⟩:=nat.exists_eq_succ_of_ne_zero (ne_of_gt (finset.card_pos.mpr hA)), 
+  rw [hc, nat.succ_eq_add_one] at this,
+  convert tn_le s B.card _ _,
+  exact (tsub_eq_of_eq_add this).symm,
+  rw [mem_range,  ← nat.succ_eq_add_one, ← hc],
+  exact card_lt_card hB',
+end
+
+
+-- usual-ish statement of turan upper bound
+theorem turan_ub : G.clique_free (t+2) → G.edge_finset.card ≤ turan_numb t (fintype.card α):=
+begin
+  intro h,
+  have sdG:= G.erdos_simple univ (G.clique_free_graph_imp_set h),
+  simp only [deg_res_univ] at sdG,
+  rwa [sum_degrees_eq_twice_card_edges,card_univ,mul_le_mul_left] at sdG, by norm_num,
+end
+
+
 
 end simple_graph
